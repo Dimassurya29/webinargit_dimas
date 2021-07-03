ce65bfcb2acdede33abba22c945f72dfdb33df6f
Dimassurya2903 <85242803+Dimassurya2903@users.noreply.github.com>
2021-07-03 14:47:11 +0700

    inisialisasi git

---
+++ src/webinargit_dimas/Webinargit_dimas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webinargit_dimas;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Webinargit_dimas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
